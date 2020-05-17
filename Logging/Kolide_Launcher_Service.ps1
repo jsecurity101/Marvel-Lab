ad65fd22be8517c8a6cf88438dda1b4ec1ca9ae9
Jsecurity101 <29631806+jsecurity101@users.noreply.github.com>
2020-05-17 11:18:49 -0700

    Automating OSQuery build - this took too long

---
+++ Logging/Kolide_Launcher_Service.ps1
@@ -0,0 +1,2 @@
+New-Service -Name "kolide_launcher" -BinaryPathName "C:\OSQuery\windows\launcher.exe --hostname=OSQuery_IP:8443 --enroll_secret=KolideSecret --osqueryd_path=C:\OSQuery\windows\osqueryd.exe  --insecure"
+
